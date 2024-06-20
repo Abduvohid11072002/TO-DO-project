649ac4f797041172e255ba7b4b2b80667c1a6463
Abduvohid <abduvohidabdurahimov1@gmail.com>
2024-06-20 09:04:15 +0500

    finished

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import { connectDatabase, connectMongoDB } from "./config/database.js";
 import mainRoute from "./routes/index.routes.js";
 import { createTables } from "./models/postgres.tables.js";
 import { logMiddleware } from "./middlewares/logs.middleware.js";
+import { logger } from "./utils/logger.js";
 
 export const app = express();
 connectDatabase();
@@ -16,14 +17,14 @@ app.use(logMiddleware);
 app.use("/", mainRoute);
 
 process.on("unhandledRejection", (reason, promise) => {
+  logger.error(`Unhandled Rejection at: ${promise}, reason: ${reason}`);
   console.error(reason);
-
   process.exit(1);
 });
 
 process.on("uncaughtException", (err) => {
+  logger.error(`Uncaught Exception thrown: ${err.message}`);
   console.error(err);
-
   process.exit(1);
 });
 
